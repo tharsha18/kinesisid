97ea3320f41d5d7cf572602baf54374d603b9d3e
tharsha18 <50037595+tharsha18@users.noreply.github.com>
2020-06-16 15:54:39 -0400

    Add files via upload

---
+++ reportStatus.py
@@ -0,0 +1,101 @@
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import sys
+import logging
+import time
+import getopt
+import random
+import json
+
+# Custom MQTT message callback
+def customCallback(client, userdata, message):
+	print(message.payload.decode('ascii'))
+
+# Read in command-line parameters
+useWebsocket = False
+host = ""
+rootCAPath = ""
+certificatePath = ""
+privateKeyPath = ""
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "hwe:k:c:r:", ["help", "endpoint=", "key=","cert=","rootCA=", "websocket"])
+	if len(opts) == 0:
+		raise getopt.GetoptError("No input parameters!")
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			print(helpInfo)
+			exit(0)
+		if opt in ("-e", "--endpoint"):
+			host = arg
+		if opt in ("-r", "--rootCA"):
+			rootCAPath = arg
+		if opt in ("-c", "--cert"):
+			certificatePath = arg
+		if opt in ("-k", "--key"):
+			privateKeyPath = arg
+		if opt in ("-w", "--websocket"):
+			useWebsocket = True
+except getopt.GetoptError:
+	print(usageInfo)
+	exit(1)
+
+# Missing configuration notification
+missingConfiguration = False
+if not host:
+	print("Missing '-e' or '--endpoint'")
+	missingConfiguration = True
+if not rootCAPath:
+	print("Missing '-r' or '--rootCA'")
+	missingConfiguration = True
+if not useWebsocket:
+	if not certificatePath:
+		print("Missing '-c' or '--cert'")
+		missingConfiguration = True
+	if not privateKeyPath:
+		print("Missing '-k' or '--key'")
+		missingConfiguration = True
+if missingConfiguration:
+	exit(2)
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.NOTSET)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Init AWSIoTMQTTClient
+myAWSIoTMQTTClient = None
+if useWebsocket:
+	myAWSIoTMQTTClient = AWSIoTMQTTClient("myIoTTopic", useWebsocket=True)
+	myAWSIoTMQTTClient.configureEndpoint(host, 443)
+	myAWSIoTMQTTClient.configureCredentials(rootCAPath)
+else:
+	myAWSIoTMQTTClient = AWSIoTMQTTClient("myIoTTopic")
+	myAWSIoTMQTTClient.configureEndpoint(host, 8883)
+	myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Connect and subscribe to AWS IoT
+myAWSIoTMQTTClient.connect()
+myAWSIoTMQTTClient.subscribe("itemStatusTopic", 0, customCallback)
+
+
+# Publish to the same topic in a loop forever
+loopCount = 0
+while True:
+	publishdata = {}
+	publishdata['customerId'] = str(random.randint(1,10))
+	publishdata['timeStamp'] = str(time.time())
+	publishdata['ItemId'] = str(random.randint(1,163))
+	publishdata['status'] = str(random.randint(0,1))
+	json_data = json.dumps(publishdata)
+	myAWSIoTMQTTClient.publish("itemStatusTopic", json_data, 1)
+	loopCount += 1
+	time.sleep(.1)
